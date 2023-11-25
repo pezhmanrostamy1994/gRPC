b91bd51152933f7fb91b4010c85098126b37640b
pezhman1994 <pezhmanheidaryrostamy1994@gmail.com>
2023-11-25 16:58:09 +0330

    refactor: rename getProductData to invokeGrpcMethod

--- client/controllers/product.controller.js
+++ client/controllers/product.controller.js
@@ -1,77 +1,89 @@
 const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const path = require("path");
-const productProtoPath = path.join(__dirname, "..", "..", "protos", "product.proto");
+const productProtoPath = path.join(
+  __dirname,
+  "..",
+  "..",
+  "protos",
+  "product.proto"
+);
 const productProto = protoLoader.loadSync(productProtoPath);
 const { productPackage } = grpc.loadPackageDefinition(productProto);
 const productServiceURL = "localhost:4001";
-const productClient = new productPackage.ProductService(productServiceURL, grpc.credentials.createInsecure());
+const productClient = new productPackage.ProductService(
+  productServiceURL,
+  grpc.credentials.createInsecure()
+);
 
 async function listProduct(req, res, next) {
-    try {
-        const data = await getProductData(productClient.listProduct);
-        return res.json(data);
-    } catch (err) {
-        return res.json(err);
-    }
+  try {
+    const data = await callGrpcMethod(productClient.listProduct);
+    return res.json(data);
+  } catch (err) {
+    return res.json(err);
+  }
 }
 
 async function getProduct(req, res, next) {
-    try {
-        const { id } = req.params;
-        const data = await getProductData(productClient.getProduct, { id });
-        return res.json(data);
-    } catch (err) {
-        return res.json(err);
-    }
+  try {
+    const { id } = req.params;
+    const data = await callGrpcMethod(productClient.getProduct, { id });
+    return res.json(data);
+  } catch (err) {
+    return res.json(err);
+  }
 }
 
 async function createProduct(req, res, next) {
-    try {
-        const { title, price } = req.body;
-        const data = await getProductData(productClient.createProduct, { title, price });
-        return res.json(data);
-    } catch (err) {
-        return res.json(err);
-    }
+  try {
+    const { title, price } = req.body;
+    const data = await callGrpcMethod(productClient.createProduct, {
+      title,
+      price,
+    });
+    return res.json(data);
+  } catch (err) {
+    return res.json(err);
+  }
 }
 
 async function updateProduct(req, res, next) {
-    try {
-        const data = req.body;
-        const result = await getProductData(productClient.updateProduct, data);
-        return res.json(result);
-    } catch (err) {
-        return res.json(err);
-    }
+  try {
+    const data = req.body;
+    const result = await callGrpcMethod(productClient.updateProduct, data);
+    return res.json(result);
+  } catch (err) {
+    return res.json(err);
+  }
 }
 
 async function deleteProduct(req, res, next) {
-    try {
-        const { id } = req.params;
-        const result = await getProductData(productClient.deleteProduct, { id });
-        return res.json(result);
-    } catch (err) {
-        return res.json(err);
-    }
+  try {
+    const { id } = req.params;
+    const result = await callGrpcMethod(productClient.deleteProduct, { id });
+    return res.json(result);
+  } catch (err) {
+    return res.json(err);
+  }
 }
 
-async function getProductData(method, requestData) {
-    return new Promise((resolve, reject) => {
-        method(requestData, (err, data) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(data);
-            }
-        });
+async function callGrpcMethod(method, requestData) {
+  return new Promise((resolve, reject) => {
+    method(requestData, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
     });
+  });
 }
 
 module.exports = {
-    deleteProduct,
-    updateProduct,
-    createProduct,
-    getProduct,
-    listProduct,
+  deleteProduct,
+  updateProduct,
+  createProduct,
+  getProduct,
+  listProduct,
 };
